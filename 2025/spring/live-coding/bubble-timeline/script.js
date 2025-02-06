3b975ffb0f107166e506014ce5a378d3e22358c9
Vinicius Sueiro <hi@vsueiro.com>
2025-02-06 00:13:45 -0500

    Add comments and clean code

--- 2025/spring/live-coding/bubble-timeline/script.js
+++ 2025/spring/live-coding/bubble-timeline/script.js
@@ -1,18 +1,25 @@
+// Import d3 library
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+// Read data
 let seizures = await d3.csv("mochi-seizures.csv", d3.autoType);
 
+// Convert duration into 0-1 values
 let scale = d3.scaleLinear().domain([20, 83]).range([0, 1]);
+
+// Get color based on 0-1 values
 let color = d3.scaleSequential(d3.interpolateBuPu);
+
+// [TEMP] Randomize horizontal position of circle
 let randomX = d3.randomUniform(0, 600);
 
-color(0.5);
-
+// For each row in my dataset
 for (let seizure of seizures) {
+  // Add a circle to my SVG chart
   chart.innerHTML += `<circle 
     cx="${randomX()}"
     cy="200"
     r="50"
-    style="fill: ${color(scale(seizure.Duration))}"
+    fill="${color(scale(seizure.Duration))}"
     ></circle>`;
 }
