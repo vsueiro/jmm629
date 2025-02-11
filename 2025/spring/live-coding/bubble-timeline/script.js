ef897ca2b27b2f372f104048a004bfc34541d787
Vinicius Sueiro <hi@vsueiro.com>
2025-02-11 17:47:48 -0500

    Add x scale

--- 2025/spring/live-coding/bubble-timeline/script.js
+++ 2025/spring/live-coding/bubble-timeline/script.js
@@ -10,14 +10,17 @@ let scale = d3.scaleLinear().domain([20, 83]).range([0, 1]);
 // Get color based on 0-1 values
 let color = d3.scaleSequential(d3.interpolateBuPu);
 
-// [TEMP] Randomize horizontal position of circle
-let randomX = d3.randomUniform(0, 600);
+// let xScale = d3.scaleTime([new Date("2025-01-01"), new Date()], [0, 600]);
+let xScale = d3
+  .scaleTime()
+  .domain([new Date("2025-01-01"), new Date()])
+  .range([0, 600]);
 
 // For each row in my dataset
 for (let seizure of seizures) {
   // Add a circle to my SVG chart
   chart.innerHTML += `<circle 
-    cx="${randomX()}"
+    cx="${xScale(seizure.Date)}"
     cy="200"
     r="50"
     fill="${color(scale(seizure.Duration))}"
