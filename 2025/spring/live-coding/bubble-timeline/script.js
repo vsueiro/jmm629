9aafc9dc34791497c544ba17b4403527ef982fda
Vinicius Sueiro <hi@vsueiro.com>
2025-02-21 13:15:49 -0500

    Update mochi chart

--- 2025/spring/live-coding/bubble-timeline/script.js
+++ 2025/spring/live-coding/bubble-timeline/script.js
@@ -5,7 +5,7 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 let seizures = await d3.csv("mochi-seizures.csv", d3.autoType);
 
 // Convert duration into 0-1 values
-let scale = d3.scaleLinear().domain([20, 83]).range([0, 1]);
+let scale = d3.scaleLinear().domain([40, 83]).range([0.25, 1]);
 
 // Get color based on 0-1 values
 let color = d3.scaleSequential(d3.interpolateBuPu);
@@ -18,8 +18,7 @@ let xScale = d3
 
 let yScale = d3.scaleLinear().domain([0, 24]).range([400, 0]);
 
-// let radiusScale = d3.scaleSqrt().domain([0, 83]).range([0, 50]);
-let radiusScale = d3.scaleLinear().domain([0, 83]).range([0, 100]);
+let radiusScale = d3.scaleSqrt().domain([0, 83]).range([0, 60]);
 
 // For each row in my dataset
 for (let seizure of seizures) {
